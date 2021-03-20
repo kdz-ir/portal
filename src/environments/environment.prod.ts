f184059733a50a613956da14ddfbf2a9986cf0b8
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-03-21 01:09:55 +0330

    add env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+import { version } from '../../package.json';
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://localhost:5000',
+  hcaptcha: '10000000-ffff-ffff-ffff-000000000001',
+  accessToken: 'access_token',
+  refreshToken: 'refresh_token',
+  vapid_key: '',
+  version,
+  name: 'پنل کانون',
 };
