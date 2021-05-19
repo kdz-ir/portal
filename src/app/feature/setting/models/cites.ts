a818b593d189a62ab18dfba9502aee7835d482f6
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-05-19 14:19:52 +0430

    fix

--- src/app/feature/setting/models/cites.ts
+++ src/app/feature/setting/models/cites.ts
@@ -1,9 +1,10 @@
 export const cites=[
-    { value: 0, lable: 'تهران' },
-    { value: 1, lable: 'یزد' },
-    { value: 2, lable: 'شیراز' },
-    { value: 3, lable: 'اصفهان' },
-    { value: 4, lable: 'کرمان' },
-    { value: 5, lable: 'کرج' },
-    { value: 6, lable: 'اهواز' },
+    { value: 1, lable: 'تهران' },
+    { value: 2, lable: 'یزد' },
+    { value: 3, lable: 'شیراز' },
+    { value: 4, lable: 'اصفهان' },
+    { value: 5, lable: 'کرمان' },
+    { value: 6, lable: 'کرج' },
+    { value: 7, lable: 'اهواز' },
+    { value: 8, lable: 'زاهدان' },
 ]
