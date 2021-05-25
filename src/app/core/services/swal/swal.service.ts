beebea36a6413258eb7709f322d78c6a8b135348
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-05-25 19:55:49 +0430

    fix state

--- src/app/core/services/swal/swal.service.ts
+++ src/app/core/services/swal/swal.service.ts
@@ -28,7 +28,8 @@ export class SwalService {
     await this.swal.fire({
       title: 'ثبت نام شما با موفقیت انجام شد.',
       text: 'منتظر دریافت پیامک باشید در صورت عدم دریافت دوباره تلاش کنید.',
-      icon: 'success'
+      icon: 'success',
+      confirmButtonText:'متوجه شدم'
     });
   }
   async accessRegister(){
