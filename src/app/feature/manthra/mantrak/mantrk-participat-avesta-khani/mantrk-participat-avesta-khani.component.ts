af09fdadf30daad2b023bf705794286feefedc96
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-05-28 02:53:23 +0430

    full mantrak

--- src/app/feature/manthra/mantrak/mantrk-participat-avesta-khani/mantrk-participat-avesta-khani.component.ts
+++ src/app/feature/manthra/mantrak/mantrk-participat-avesta-khani/mantrk-participat-avesta-khani.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SwalService } from 'src/app/core/services/swal/swal.service';
+import { ManthraReporsitoryService } from '../../services/manthra-reporsitory.service';
 
 @Component({
   selector: 'app-mantrk-participat-avesta-khani',
   templateUrl: './mantrk-participat-avesta-khani.component.html',
   styleUrls: ['./mantrk-participat-avesta-khani.component.scss']
 })
-export class MantrkParticipatAvestaKhaniComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MantrkParticipatAvestaKhaniComponent {
+  sectionOfManthra = sectionOfManthra;
+  radeOfAvestaKhani = radeOfAvestaKhani;
+  participatAvastaForm: FormGroup;
+  get isContainMAvestaKhani(): boolean {
+    return (<Array<number>>this.participatAvastaForm.value.sectionOfManthra)?.includes(2) || (<Array<number>>this.participatAvastaForm.value.sectionOfManthra)?.includes(0);
   }
+  constructor (private readonly _fb: FormBuilder, private readonly _repository: ManthraReporsitoryService, private readonly _swal: SwalService, private readonly _router: Router) {
+    this.participatAvastaForm = _fb.group({
+      numberofmanthras: [, [Validators.required]],
+      goalForOld: [, [Validators.required]],
+      sectionOfManthra: [, [Validators.required]],
+      radeOfAvestaKhani: [],
+      isAcceptInfinal: [false],
+      finalPlace: [],
+      isRegisterdLastMantrak: [false],
+      likeInLastManthrak: [0]
+    });
+  }
+  onSubmit() {
+    const formValue = this.participatAvastaForm.value;
+    const data = {
+      numberofmanthras: formValue.numberofmanthras,
+      goalForOld: formValue.goalForOld,
+      sectionOfManthra: formValue.sectionOfManthra,
+      radeOfAvestaKhani: formValue.radeOfAvestaKhani,
+      isAcceptInfinal: formValue.isAcceptInfinal,
+      finalPlace: formValue.finalPlace,
+      isRegisterdLastMantrak: formValue.isRegisterdLastMantrak,
+      likeInLastManthrak: formValue.likeInLastManthrak,
+      likedLastManthrak: formValue.likedLastManthrak,
+      step: 100,
+      status: 'end'
+    };
+    this._repository.mantrakRegister(data).subscribe(c => {
+      this._swal.successFullRegister();
+      this._router.navigate(['/']);
 
+    });
+  }
 }
+export const sectionOfManthra = [
+  { value: 0, label: 'بخش اوستاخوانی و مانترک' },
+  { value: 1, label: 'بخش مانترک' },
+  { value: 2, label: 'بخش اوستاخوانی' },
+];
+export const radeOfAvestaKhani = [
+  { value: 0, label: 'اهنودگات' },
+  { value: 1, label: 'از برخوانی ۱۳ تا ۱۸ سال' },
+  { value: 2, label: 'اشتود-سپنتمدگات' },
+  { value: 3, label: 'کل گاتا' },
+  { value: 4, label: 'از برخوانی گروهی' },
+
+];
