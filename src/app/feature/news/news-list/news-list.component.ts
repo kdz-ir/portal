a9eebddef6be8ebda2ee844931560f6cf7211152
raity <raytivojdaniirani@gmail.com>
2023-05-31 21:27:46 +0430

    share function correction

--- src/app/feature/news/news-list/news-list.component.ts
+++ src/app/feature/news/news-list/news-list.component.ts
@@ -10,24 +10,19 @@ declare const navigator: Navigator;
   styleUrls: ['./news-list.component.scss']
 })
 export class NewsListComponent {
-  newsList: NewsList;
-  readonly isWebShareSupported: boolean=navigator && navigator.share ? true : false;
-newsList2=this._newsRepository.getNewsList;
+ 
+readonly isWebShareSupported: boolean=navigator && navigator.share ? true : false;
+newsList=this._newsRepository.getNewsList;
   constructor(private readonly _newsRepository: NewsRepositoryService) {}
 
-
-  
-
   share(title: string, url: string) {
-    if (navigator && navigator.share) {
-      navigator.share({
-        title: title,
-        url: url,
-      })
-        .then(() => console.log('Successful share'))
-        .catch((error) => console.log('Error sharing:', error));
-    } else {
-      console.log('Web Share API not supported');
-    }
+    navigator.share({
+      title: title,
+      text:`برای خواندن خبر '${title}' برای خواندن ادامه مطلب روی لینک بزن!`,
+      url: url
+    })
+      .catch((error) => console.log('Error sharing:', error));
   }
-}
+  
+  }
+
