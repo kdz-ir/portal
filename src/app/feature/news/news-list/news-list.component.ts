ba3bbb9cdb247dfe1d34777805fe03412cc50db6
raity <raytivojdaniirani@gmail.com>
2023-05-31 14:54:36 +0430

    dont show share icon if browser doesnt support

--- src/app/feature/news/news-list/news-list.component.ts
+++ src/app/feature/news/news-list/news-list.component.ts
@@ -3,34 +3,40 @@ import { NewsRepositoryService } from '../services/news-repository.service';
 import { NewsList } from '../model/news-list';
 import { DomSanitizer } from '@angular/platform-browser';
 declare const navigator: Navigator;
+
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.scss']
 })
 export class NewsListComponent implements AfterViewInit {
-  newsList:NewsList;
+  newsList: NewsList;
+  isWebShareSupported: boolean;
+
+  constructor(private readonly _newsRepository: NewsRepositoryService) {}
+
   ngAfterViewInit(): void {
-   this._newsRepository.getNewsList().subscribe(c=>{
-    this.newsList=c;
-    console.log(c.posts)
-   })
+    this._newsRepository.getNewsList().subscribe(c => {
+      this.newsList = c;
+      console.log(c.posts);
+      this.checkWebShareSupport();
+    });
   }
 
-constructor(private readonly _newsRepository:NewsRepositoryService,private sanitizer: DomSanitizer) {}
-share(title:string,url:string) {
-  if (navigator && navigator.share) {
-    navigator.share({
-      title: title,
-      url: url,
-    })
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing:', error));
-  } else {
-    console.log('Web Share API not supported');
+  checkWebShareSupport() {
+    this.isWebShareSupported = navigator && navigator.share ? true : false;
   }
-}
 
-
-
+  share(title: string, url: string) {
+    if (navigator && navigator.share) {
+      navigator.share({
+        title: title,
+        url: url,
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing:', error));
+    } else {
+      console.log('Web Share API not supported');
+    }
+  }
 }
