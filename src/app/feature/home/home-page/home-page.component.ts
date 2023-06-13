6083b0f060716a07cc101a7e45cfe3a27e27c7f7
behrooz bozorg chami <behroozbc@outlook.com>
2023-06-13 22:37:10 +0330

    fix multi unrequired requests bug

--- src/app/feature/home/home-page/home-page.component.ts
+++ src/app/feature/home/home-page/home-page.component.ts
@@ -1,41 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { gataHaRegisterFilledEntity } from 'src/app/core/model/gata-ha-register-filled';
-import { SwalService } from 'src/app/core/services/swal/swal.service';
-import { CoreProfileService } from 'src/app/core/services/user/core-profile.service';
-import { gataHaRegisterFiled } from '../../manthra/models/gata-ha-register-field-enum';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements AfterViewInit {
-  profileStatus: boolean = false;
-  registerFiled: gataHaRegisterFilledEntity;
-  constructor (private readonly _coreProfileService: CoreProfileService, private readonly _swal: SwalService) { }
-  ngAfterViewInit(): void {
-    this._coreProfileService.getUserRegisteredFilled().subscribe(c => {
-      this.registerFiled = c.entity;
-    });
-    this._coreProfileService.checkProfileStatus().subscribe(c => this.profileStatus = c.status);
-  }
-  onSeeRegisters() {
-    const regs = this.registerFiled.registerFiled.map(c => {
-      switch (c) {
-        case gataHaRegisterFiled.projectBase:
-          return 'پروژه محور';
-        case gataHaRegisterFiled.Asli:
-          return 'آزاد بخش اصلی';
-        case gataHaRegisterFiled.Janbi:
-          return 'آزاد بخش جنبی';
-      }
-    });
-    this._swal.swal.fire({
-      title: 'کاربر گرامی؛ شما در این بخش‌ها ثبت ‌نام کرده‌اید.',
-      html: regs.join('<br>'),
-      icon: 'info',
-      confirmButtonText: 'بستن'
-    });
-  }
+export class HomePageComponent {
 
 }
