ceb8d467176f8f2da1c5b8f6e85065e78f546546
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-18 10:05:04 +0330

    totalAnalysis

--- src/app/feature/jam/modules/setad/services/setad-repository.service.ts
+++ src/app/feature/jam/modules/setad/services/setad-repository.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IDataCollection } from '../../shared/jam-shared/models/IDataCollection';
 import { IPlayer } from '../../shared/jam-shared/models/IPlayer';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { SportField } from '../../../models/sport-field';
 import { SportSubField } from '../../../models/sub-sport-field';
 import { IDoubleTeam } from '../models/idouble-team';
 import { IDoubleTeamDetial } from '../models/idouble-team-detial';
 import { ITeamsList } from '../models/iteams-list';
 import { ITeamDetail } from '../models/iteam-detail';
+import { timer } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -18,6 +19,9 @@ export class SetadRepositoryService {
   constructor (private readonly _httpClient: HttpClient) { }
   get allPlayers() {
     return this._httpClient.get<IDataCollection<IPlayer>>(this._url + '/all').pipe(map(c => c.data));
+  }
+  get totalAnalysis() {
+    return timer(300, 5000).pipe(switchMap(t => this._httpClient.get<{ total: number, unChecked: number; }>(this._url + '/analysis')));
   }
   singlePlayers(field: SportField, subField: SportSubField) {
     return this._httpClient.post<IDataCollection<IPlayer>>(`${this._url}/single-filtered`, { field, subField })
@@ -39,4 +43,5 @@ export class SetadRepositoryService {
   doubleTeam(id: number) {
     return this._httpClient.get<IDoubleTeamDetial>(`${this._url}/double/${id}`);
   }
+
 }
