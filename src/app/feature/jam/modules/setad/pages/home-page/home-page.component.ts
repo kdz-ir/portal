a3812aeecae0afe5fcfbcb4e58fd25be91dd8bc9
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-18 12:43:51 +0330

    fix api call

--- src/app/feature/jam/modules/setad/pages/home-page/home-page.component.ts
+++ src/app/feature/jam/modules/setad/pages/home-page/home-page.component.ts
@@ -1,17 +1,23 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { SetadRepositoryService } from '../../services/setad-repository.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent  {
+export class HomePageComponent implements OnDestroy {
   totalAnalysis: { total: number; unChecked: number; };
-  
+  _unsubscribe: Subject<void> = new Subject<void>();
   constructor (private readonly _setadRepository: SetadRepositoryService) {
-    _setadRepository.totalAnalysis.subscribe(c=>{
+    _setadRepository.totalAnalysis.pipe(takeUntil(this._unsubscribe)).subscribe(c=>{
       this.totalAnalysis=c;
     })
+  }
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
   }
 }
