9e0774d4d8bf37f0e2b39ae2505844b318bf54c6
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-19 16:14:25 +0330

    change redirect path

--- src/app/feature/jam/modules/setad/pages/single/single-player-ditail-page/single-player-ditail-page.component.ts
+++ src/app/feature/jam/modules/setad/pages/single/single-player-ditail-page/single-player-ditail-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IPlayer } from '../../../../shared/jam-shared/models/IPlayer';
 import { SetadRepositoryService } from '../../../services/setad-repository.service';
+import { Location } from '@angular/common';
+import { SwalService } from 'src/app/core/services/swal/swal.service';
 
 @Component({
   selector: 'app-single-player-ditail-page',
@@ -10,10 +12,14 @@ import { SetadRepositoryService } from '../../../services/setad-repository.servi
 })
 export class SinglePlayerDitailPageComponent {
   player: IPlayer;
-  constructor (ac: ActivatedRoute, private readonly _setadRepository: SetadRepositoryService) {
+  constructor (ac: ActivatedRoute, private readonly _setadRepository: SetadRepositoryService,
+     private readonly _location: Location, private readonly _swal: SwalService) {
     this.player = <IPlayer>ac.snapshot.data['player'];
   }
   onVerifyRegister() {
-    this._setadRepository.verifySingle(this.player.id, this.player.field, this.player.subField).subscribe(t => console.log(t));
+    this._setadRepository.verifySingle(this.player.id, this.player.field, this.player.subField).subscribe(t => {
+      this._swal.successFullSubmited();
+      this._location.back();
+    });
   }
 }
