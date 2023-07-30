61272e476db626076b536c63a3fca4323ad5332d
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-30 17:29:27 +0330

    team export

--- src/app/feature/jam/modules/setad/pages/teams/team-detail-page/team-detail-page.component.ts
+++ src/app/feature/jam/modules/setad/pages/teams/team-detail-page/team-detail-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ITeamDetail } from '../../../models/iteam-detail';
+import { saveAs } from 'file-saver';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-team-detail-page',
@@ -8,10 +10,12 @@ import { ITeamDetail } from '../../../models/iteam-detail';
   styleUrls: ['./team-detail-page.component.scss']
 })
 export class TeamDetailPageComponent {
-team: ITeamDetail;
+  team: ITeamDetail;
 
-constructor(ac:ActivatedRoute) {
-  this.team=<ITeamDetail>ac.snapshot.data['team'];
-  
-}
+  constructor (ac: ActivatedRoute) {
+    this.team = <ITeamDetail>ac.snapshot.data['team'];
+  }
+  onExportList(){
+    saveAs(new Blob([JSON.stringify(this.team)]),`${this.team.name}-${this.team.id}-${moment().format('jDD-HH-mm-ss')}.json`)
+  }
 }
