fbb0c4c2e5c1112988d089450434d27b3422d250
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-30 21:51:46 +0330

    drom export

--- src/app/feature/jam/modules/setad/pages/dormitory/dormitory-list-page/dormitory-list-page.component.ts
+++ src/app/feature/jam/modules/setad/pages/dormitory/dormitory-list-page/dormitory-list-page.component.ts
@@ -2,7 +2,10 @@ import { AfterViewInit, Component } from '@angular/core';
 import { SetadRepositoryService } from '../../../services/setad-repository.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { IDormitories } from '../../../models/idormitories';
-
+import { Workbook } from 'exceljs';
+import * as jalaliMoment from 'jalali-moment';
+import * as saveAs from 'file-saver';
+import { isEmpty } from 'lodash-es';
 @Component({
   selector: 'app-dormitory-list-page',
   templateUrl: './dormitory-list-page.component.html',
@@ -11,10 +14,23 @@ import { IDormitories } from '../../../models/idormitories';
 export class DormitoryListPageComponent implements AfterViewInit {
   dataSource: MatTableDataSource<IDormitories>;
   displayedColumns: string[] = ['name', 'sex', 'nationalCode', 'city', 'refId', 'type', 'authority', 'created', 'action'];
+  data: IDormitories[];
   constructor (private readonly _setadRepository: SetadRepositoryService) {
 
   }
   ngAfterViewInit(): void {
-    this._setadRepository.dormitories.subscribe(c => this.dataSource = new MatTableDataSource(c));
+    this._setadRepository.dormitories.subscribe(c => {
+      this.data = c;
+      this.dataSource = new MatTableDataSource(c);
+    });
+  }
+  async onDownloadExport() {
+    const wb = new Workbook();
+    const ws = wb.addWorksheet('dormitory');
+    const cols = [{ name: 'نام و نام خانوادگی' }, { name: 'امکانات' }, { name: 'جنسیت' }, { name: 'شهر' }, { name: 'کد ملی' }, { name: 'شماره پرداخت' }];
+    const row = this.data.filter(c=>!isEmpty(c.refId)).map(d => ([d.profile.name + ' ' + d.profile.family, d.type, d.profile.sex, d.profile.city, d.nationalCode, d.refId]));
+    ws.addTable({ name: 'mytable', headerRow: true, ref: 'A1', rows: row, columns: cols });
+    ws.columns?.forEach(c => c.width = 25);
+    saveAs(new Blob([await wb.xlsx.writeBuffer()]), `dormitory-${jalaliMoment().format('jYYYYjMMjDD-HHmm')}.xlsx`);
   }
 }
