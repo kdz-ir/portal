82c0b75875c878933586b872f98d79150fa3540b
behrooz bozorg chami <behroozbc@outlook.com>
2023-07-31 18:36:33 +0330

    download card

--- src/app/feature/jam/modules/setad/pages/tools/cards-list-page/cards-list-page.component.ts
+++ src/app/feature/jam/modules/setad/pages/tools/cards-list-page/cards-list-page.component.ts
@@ -3,6 +3,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { IPlayer } from '../../../../shared/jam-shared/models/IPlayer';
 import { SetadRepositoryService } from '../../../services/setad-repository.service';
 import { ICard } from '../../../models/icard';
+import * as saveAs from 'file-saver';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-cards-list-page',
@@ -12,15 +14,19 @@ import { ICard } from '../../../models/icard';
 export class CardsListPageComponent {
   dataSource: MatTableDataSource<ICard>;
   displayedColumns: string[] = ['name', 'nationalCode', 'ageRangeName','fieldName','city'];
+  data: ICard[];
   constructor(private _setadRepository:SetadRepositoryService) {
     
   }
   ngAfterViewInit(): void {
     this.onRefreshClick();
   }
-
+  onDownloadExport(){
+    saveAs(new Blob([JSON.stringify(this.data)]), `${moment().format('jDD-HH-mm-ss')}.json`);
+  }
   onRefreshClick() {
     this._setadRepository.cards.subscribe(c => {
+      this.data=c;
       this.dataSource = new MatTableDataSource(c);
     });
   }
