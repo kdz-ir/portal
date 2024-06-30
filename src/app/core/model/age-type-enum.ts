423eec8a9a05d7942663ca4ba59ef8f523fb8e9b
Hootan Hemmati <hootanhemmati@outlook.com>
2024-06-30 20:04:14 +0330

    Generate enum for age type

--- src/app/core/model/age-type-enum.ts
+++ src/app/core/model/age-type-enum.ts
@@ -1,6 +1,17 @@
+/**
+ * Enum for age types with values representing different age groups.
+ */
 export enum AgeType {
-    Pulse18 = 1,
-    between13and18 = 2,
+    /**
+     * Represents individuals who are 18 years old or older.
+     */
+    plus18 = 1,
+    /**
+     * Represents individuals who are between 13 and 18 years old.
+     */
+    between13And18 = 2,
+    /**
+     * Represents individuals who are under 13 years old.
+     */
     under13 = 3
 }
-
