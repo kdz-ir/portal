c3dbdf5b82ae333dff368e09eb864abec904dce8
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-25 09:07:14 +0200

    fix

--- src/app/feature/jam/modules/setad/models/idormitories.ts
+++ src/app/feature/jam/modules/setad/models/idormitories.ts
@@ -1,6 +1,7 @@
 import { ProfileShowEntity } from "src/app/feature/setting/models/profile-info";
 
 export interface IDormitories {
+    id:number;
     profile: ProfileShowEntity;
     refId: string;
     authority: string;
