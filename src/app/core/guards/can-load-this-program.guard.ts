a9812a1b54c0bfe378a26d1b42fdd535aa143139
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-05-28 16:19:21 +0330

    the admin (with register permission) can now register if registration is off.

--- src/app/core/guards/can-load-this-program.guard.ts
+++ src/app/core/guards/can-load-this-program.guard.ts
@@ -2,11 +2,18 @@ import { Injectable, inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SwalService } from '../services/swal/swal.service';
+import { AuthenticationService } from "src/app/core/services/authentication/authentication.service";
+
 
 export function CanLoadThisProgramGuard(can: boolean): CanActivateFn {
   return () => {
-    if (!can)
-      inject(SwalService).showErrorMessage('در حال حاظر دسترسی به این برنامه محدود شده است.');
+    if (!can) {
+      var canRegister = inject(AuthenticationService).getTokenItem<number>("registerPermission") == 1;
+      can = canRegister || can;
+      if (!canRegister) {
+        inject(SwalService).showErrorMessage('در حال حاظر دسترسی به این برنامه محدود شده است.');
+      }
+    }
     return can;
   };
 }
